36996f2577e5c31acd39a6c0468be9323d56bee9
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-06-03 13:24:09 -0500

    Ajustes

--- src/main/java/com/sofkau/laboratorio/Main.java
+++ src/main/java/com/sofkau/laboratorio/Main.java
@@ -3,6 +3,6 @@ import org.jboss.logging.Logger;
 public class Main {
     static final Logger logger = Logger.getLogger("logger");
     public static void main(String[] args) {
-                
+
     }
 }
